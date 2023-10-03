7f8a2194bea5e266c5254cf565cef15a10e09a32
Vasyanator <135327897+Vasyanator@users.noreply.github.com>
2023-10-03 11:29:08 +0300

    add the ability not to translate specified text fragments.

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import gradio as gr
 from deep_translator import GoogleTranslator
 import json
 import os
-
+import re
 settings_path = "extensions/google_translate_plus/settings.json"
 
 default_params = {
@@ -12,6 +12,7 @@ default_params = {
     "language string": "ru",
     "translate_paragraphs_individually": True,
     "debug": False,
+    "special_symbol": "~",
 
 }
 try:
@@ -35,8 +36,8 @@ language_codes = {'Afrikaans': 'af', 'Albanian': 'sq', 'Amharic': 'am', 'Arabic'
 
 def input_modifier(string):
     if not params['Translate_user_input']:
-        if params.get('debug', False):
-            print("[Google translate plus]: Перевод входного текста отключен")
+        if params.get('debug'):
+            print("[Google translate plus]: Input text translation disabled")
         return string
     
     return translate_text(string, params['language string'], "en")
@@ -44,27 +45,41 @@ def input_modifier(string):
 
 def output_modifier(string):
     if not params['Translate_system_output']:
-        if params.get('debug', False):
-            print("[Google translate plus]: Перевод ответа модели отключен")
+        if params.get('debug'):
+            print("[Google translate plus]: Output nput text translation disabled")
         return string
     
     return translate_text(string, "en", params['language string'])
 
 def translate_text(string, sourcelang, targetlang):
-    if params.get('debug', False):
-        print(f"[Google translate plus]: Переводим следующий текст: \n{string}\n\n")
-    if params.get('translate_paragraphs_individually', False):
-        lines = string.splitlines()
-        translated_lines = [
-            str(GoogleTranslator(source=sourcelang, target=targetlang).translate(html.unescape(line))) if line.strip() else line 
-            for line in lines
-        ]
-        translated_text = "\n".join(translated_lines)
-    else:
-        translated_str = str(GoogleTranslator(source=sourcelang, target=targetlang).translate(html.unescape(string)))
-        translated_text = html.escape(translated_str)
-    if params.get('debug', False):
-        print(f"[Google translate plus]: Текст успешно переведен. Результат:\n{translated_text}\n\n")
+    if params.get('debug'):
+        print(f"[Google translate plus]: The text is currently being translated: \n{string}\n\n")
+    special_symbol = params['special_symbol']
+    fragments = re.split(f"{special_symbol}(.*?){special_symbol}", string)
+    
+    translated_fragments = []
+
+    for idx, fragment in enumerate(fragments):
+        if idx % 2 == 1:
+            translated_fragments.append(fragment)
+            continue
+
+        if params.get('translate_paragraphs_individually', False):
+            lines = fragment.splitlines()
+            translated_lines = [
+                str(GoogleTranslator(source=sourcelang, target=targetlang).translate(html.unescape(line))) if line.strip() else line 
+                for line in lines
+            ]
+            translated_fragments.append("\n".join(translated_lines))
+        else:
+            translated_str = str(GoogleTranslator(source=sourcelang, target=targetlang).translate(html.unescape(fragment)))
+            translated_fragments.append(html.escape(translated_str))
+
+    translated_text = "".join(translated_fragments).replace(special_symbol, '')
+
+    if params.get('debug'):
+        print(f"[Google translate plus]: The text has been successfully translated. Result: \n{translated_text}\n\n")
+
     return translated_text
     
 
@@ -85,6 +100,7 @@ def ui():
     with gr.Column():
         Translate_user_input = gr.Checkbox(value=params['Translate_user_input'], label='Translate user input')
         Translate_system_output = gr.Checkbox(value=params['Translate_system_output'], label='Translate system output')
+        special_symbol = gr.Textbox(value=params['special_symbol'], label='Special symbol. Text between these characters will not be translated. Some characters may cause errors.', type='text',)
         language = gr.Dropdown(value=language_name, choices=[k for k in language_codes], label='Language')
         translate_paragraphs_individually = gr.Checkbox(value=params.get('translate_paragraphs_individually', False), label='Translate paragraphs individually')
     
@@ -92,5 +108,6 @@ def ui():
     # Event functions to update the parameters in the backend
     Translate_user_input.change(lambda x: params.update({"Translate_user_input": x}) or save_params(), Translate_user_input, None)
     Translate_system_output.change(lambda x: params.update({"Translate_system_output": x}) or save_params(), Translate_system_output, None)
+    special_symbol.change(lambda x: params.update({"special_symbol": x}) or save_params(), special_symbol, None)
     language.change(lambda x: params.update({"language string": language_codes[x]}), language, None)
     translate_paragraphs_individually.change(lambda x: params.update({"translate_paragraphs_individually": x}), translate_paragraphs_individually, None)
